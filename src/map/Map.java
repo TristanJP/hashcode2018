6a84973cdf8bb95c216fd8229bf65a4ed5dfb14e
Ollie Nye <nyeo@aston.ac.uk>
2018-03-01 18:48:08 +0000

    Map structure

--- src/map/Map.java
+++ src/map/Map.java
@@ -6,5 +6,16 @@ import java.util.HashMap;
 import Types.Coordinate;
 
 public class Map {
+	
 	private HashMap<Coordinate, ArrayList<String>> map;
+	
+	public Map(Integer rows, Integer columns) {
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c < columns; c++) {
+				Coordinate coordinate = new Coordinate(r, c);
+				map.put(coordinate, new ArrayList<String>());
+			}
+		}
+	}
+	
 }
