43c44b9f54fad5988d1925491ad65966c7f4523e
Tristan Perkins <trisperk@hotmail.com>
2018-03-01 20:00:35 +0000

    working on vehicle manager

--- src/rideScheduler/VehicleManager.java
+++ src/rideScheduler/VehicleManager.java
@@ -1,16 +1,27 @@
 package rideScheduler;
 import java.util.ArrayList;
 
+import Types.Coordinate;
 import trip.Vehicle;
 
 public class VehicleManager {
-	private ArrayList<Vehicle> generatedVehicles;
+	
+	private ArrayList<Vehicle> vehicles;
+	
 	public VehicleManager(int vNum)
 	{
-		generatedVehicles = new ArrayList<Vehicle>(); 
-		for(int i = 0; i< vNum; i++)
+		vehicles = new ArrayList<Vehicle>(); 
+		for(int i = 0; i < vNum; i++)
 		{
-			generatedVehicles.add(new Vehicle());
+			vehicles.add(new Vehicle());
 		}
 	}
+	
+	public void updateList(Vehicle vehicle) {
+		Coordinate newPos = vehicle.getRide().getEndPos();
+		
+		for (int i = 0; i < vehicles.size(); i++) {
+		}
+	}
+	
 }
