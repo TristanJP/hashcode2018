6c0950333d5737b7d9b9ad70e853e21cf97daf0c
Ollie Nye <nyeo@aston.ac.uk>
2018-03-01 20:47:17 +0000

    Refactor System into RideSystem

--- src/rideScheduler/RideQueue.java
+++ src/rideScheduler/RideQueue.java
@@ -1,7 +1,9 @@
 package rideScheduler;
 
+import java.util.ArrayList;
 import java.util.concurrent.ArrayBlockingQueue;
 
+import importer.Importer;
 import trip.Ride;
 
 public class RideQueue  {
@@ -9,7 +11,7 @@ public class RideQueue  {
 	private ArrayBlockingQueue<Ride> queue;
 	
 	public RideQueue() {
-		
+		ArrayList<Ride> rides = Importer.getInstance().getRides();
 	}
 	
 	public ArrayBlockingQueue getQueue() {
