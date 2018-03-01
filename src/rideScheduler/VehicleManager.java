b285645bccbce7481299256f597f231d89bf3d4a
Tristan Perkins <trisperk@hotmail.com>
2018-03-01 20:31:15 +0000

    think ive finished list sorted

--- src/rideScheduler/VehicleManager.java
+++ src/rideScheduler/VehicleManager.java
@@ -19,14 +19,53 @@ public class VehicleManager {
 	
 	public void updateList(Vehicle vehicle) {
 		Coordinate newPos = vehicle.getRide().getEndPos();
+		int indexToInsert = 0;
 		
 		for (int i = 0; i < vehicles.size(); i++) {
+			if (vehicles.get(i).getLocation().getX() <= newPos.getX()) {
+				if (vehicles.get(i).getLocation().getY() <= newPos.getY()) {
+					indexToInsert = i;
+				}
+				else {
+					
+				}
+			}
 		}
+		
+		indexToInsert = sortX(indexToInsert, newPos);
+		indexToInsert = sortY(indexToInsert, newPos);
+		
+		vehicles.remove(vehicle);
+		vehicles.add(indexToInsert+1, vehicle);
 	}
 	
 	public ArrayList<Vehicle> getVehices()
 	{
 		return vehicles;
+	}
+	
+	public int sortX(int indexToInsert, Coordinate newPos) {
+		for (int i = indexToInsert; i < vehicles.size(); i++) {
+			if (vehicles.get(i).getLocation().getX() <= newPos.getX()) {
+				indexToInsert = i;
+			}
+			else {
+				return indexToInsert;
+			}
+		}
+		return indexToInsert;
+	}
+	
+	public int sortY(int indexToInsert, Coordinate newPos) {
+		for (int i = indexToInsert; i < vehicles.size(); i++) {
+			if (vehicles.get(i).getLocation().getY() <= newPos.getY()) {
+				indexToInsert = i;
+			}
+			else {
+				return indexToInsert;
+			}
+		}
+		return indexToInsert;
 	}
 
 }
