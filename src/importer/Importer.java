1f6ca9c40db21d51ca27a6145fa81c656034a2ca
Ollie Nye <nyeo@aston.ac.uk>
2018-03-01 21:12:09 +0000

    Change dataset

--- src/importer/Importer.java
+++ src/importer/Importer.java
@@ -30,9 +30,9 @@ public class Importer {
 	private Importer() {
 		//this("./a_example.in");
 		//this("./b_should_be_easy.in");
-		this("./c_no_hurry.in");
+		//this("./c_no_hurry.in");
 		//this("./d_metropolis.in");
-		//this("./e_high_bonus.in");
+		this("./e_high_bonus.in");
 	}
 
 	private Importer(String path) {
