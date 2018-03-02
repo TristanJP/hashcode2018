60db99bb876411abc39c342feb5fddbac31b40d5
Jordan Lees <leesja@aston.ac.uk>
2018-03-02 00:56:20 +0000

    ride contains vehicle assigned

--- src/rideScheduler/RideManager.java
+++ src/rideScheduler/RideManager.java
@@ -1,11 +1,14 @@
 package rideScheduler;
 
 import trip.*;
+import java.util.ArrayList;
 import java.util.Queue;
 import importer.*;
 
 public class RideManager implements Ticker {
 	private Queue<Ride> rideQueue;
+	private Queue<Ride> pendingRides;
+	private ArrayList<Ride> ongoingRides;
 	private VehicleManager vehicleManager;
 	
 	public RideManager(RideQueue rq, VehicleManager vm) 
